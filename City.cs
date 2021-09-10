105af00acc3427c7bd9b2309b89aedc8812c8ef4
Milos Mirkovic <milos.m96@hotmail.com>
2021-09-10 09:19:05 +0200

    first commit

---
+++ City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShop
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public List<Store> StoreList { get; set; }
+
+        public City(string name)
+        {
+            this.Name = name;
+            this.StoreList = new List<Store>();
+        }
+
+        public void AddStore(Store shop)
+        {
+            StoreList.Add(shop);
+            shop.CityName = this.Name;
+        }
+    }
+}
---
+++ Store.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShop
+{
+    public class Store
+    {
+        public string CityName { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Inventory Inventory { get; set; }
+
+        public Store(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Inventory = new Inventory();
+        }
+
+        public void AddRecord(ItemValueRecord item)
+        {
+            Inventory.AddRecord(item);
+        }
+
+        public void GroupInsertIntoTable()
+        {
+            Inventory.GroupInsertIntoTable();
+        }
+
+        public override string ToString()
+        {
+            return $"Store name: {Name}, Address: {Address} \n{Inventory}";
+        }
+    }
+}
